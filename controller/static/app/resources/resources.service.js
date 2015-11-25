48ba75a957dffd62c9fabeded88945f03fd56e17
chengjt <chengjt@csdn.net>
2015-11-25 10:49:58 +0800

    fix the resources view double click bug

--- controller/static/app/resources/resources.service.js
+++ controller/static/app/resources/resources.service.js
@@ -13,23 +13,37 @@
                 var getResList = $http
                     .get('/resources/list')
                     .then(function(response) {
-                        return response.data;
-                    });
-	var cts = $http.get("/containers/json?all=1")				
+                        	var rList = response.data;
+						
+		var p =$http.get("/containers/json?all=1")				
 		.then(function(response){
-		    return response.data;	
+			var cts = response.data;
+			var cListIDs =cts.map(item=>item.Id);
+			var data = rList.map(item=>{
+				item.ExistContainer = cListIDs.indexOf(item.ContainerID)>=0;
+				var time = new Date(item.CreateTime);
+				item.CreateTime=time.getFullYear()+"-"+time.getMonth()+"-"+time.getDate()+"   " +time.getHours()+":"+time.getMinutes()+":"+time.getSeconds();
+				return item;
+			});
+			return data;
 		});
-	var promise = Promise.all([getResList,cts]).then(function(value){
-	    var rList = value[0];
-	    var cListIDs =value[1].map(item=>item.Id);
-	    var data = rList.map(item=>{
-			item.ExistContainer = cListIDs.indexOf(item.ContainerID)>=0
-			return item;
-		});
-	    return data;
-	});
+		
+		return p;
+                    });
+
+//	var promise = Promise.all([getResList,cts]).then(function(value){
+//	    var rList = value[0];
+//	    var cListIDs =value[1].map(item=>item.Id);
+//	    var data = rList.map(item=>{
+//			item.ExistContainer = cListIDs.indexOf(item.ContainerID)>=0;
+//			var time = new Date(item.CreateTime);
+//			item.CreateTime=time.getFullYear()+"-"+time.getMonth()+"-"+time.getDate()+"   " +time.getHours()+":"+time.getMinutes()+":"+time.getSeconds();
+//			return item;
+//		});
+//	    return data;
+//	});
 	
-                return promise;
+                return getResList;
             },
             delete: function(resourceId) {
                 var promise = $http
